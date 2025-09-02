c5b622b76ce3a1626eb9859502755febd11593be
Derek <derek.spinach.murillo@est.una.ac.cr>
2025-09-02 16:53:06 -0600

    Se agrega JAXB y el fileStore Paciente

--- src/main/java/org/example/domain_layer/Paciente.java
+++ src/main/java/org/example/domain_layer/Paciente.java
@@ -1,10 +1,27 @@
 package org.example.domain_layer;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="paciente")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Paciente {
-    String ID;
-    String Nombre;
-    String Fecha_Nacimiento;
-    String Num_Telefono;
+    @XmlElement(name = "id")
+    private String ID;
+
+    @XmlElement(name = "nombre")
+    private String Nombre;
+
+    @XmlElement(name = "fecha_nacimiento")
+    private String Fecha_Nacimiento;
+
+    @XmlElement(name = "telefono")
+    private String Num_Telefono;
+
+    public Paciente() {} // requerido por JAXB
+
     public Paciente(String ID, String Nombre, String Fecha_Nacimiento, String Num_Telefono) {
         this.ID = ID;
         this.Nombre = Nombre;
@@ -12,36 +29,12 @@ public class Paciente {
         this.Num_Telefono = Num_Telefono;
     }
 
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getFecha_Nacimiento() {
-        return Fecha_Nacimiento;
-    }
-
-    public void setFecha_Nacimiento(String fecha_Nacimiento) {
-        Fecha_Nacimiento = fecha_Nacimiento;
-    }
-
-    public String getNum_Telefono() {
-        return Num_Telefono;
-    }
-
-    public void setNum_Telefono(String num_Telefono) {
-        Num_Telefono = num_Telefono;
-    }
-
+    public String getID() { return ID; }
+    public void setID(String ID) { this.ID = ID; }
+    public String getNombre() { return Nombre; }
+    public void setNombre(String nombre) { Nombre = nombre; }
+    public String getFecha_Nacimiento() { return Fecha_Nacimiento; }
+    public void setFecha_Nacimiento(String fecha_Nacimiento) { Fecha_Nacimiento = fecha_Nacimiento; }
+    public String getNum_Telefono() { return Num_Telefono; }
+    public void setNum_Telefono(String num_Telefono) { Num_Telefono = num_Telefono; }
 }
